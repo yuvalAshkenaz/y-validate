df39a794ecfabbd9d1d66399e470a9561ba2e329
yuvalAshkenaz <110046564+yuvalAshkenaz@users.noreply.github.com>
2024-09-08 13:18:49 +0300

    Add files via upload

--- y-validate.js
+++ y-validate.js
@@ -1,6 +1,6 @@
-/*! y-validate - v1.3 - 19/06/2024
+/*! y-validate - v2.0 - 08/09/2024
 * By Yuval Ashkenazi */
-jQuery('head').append('<style  type="text/css">input.error,textarea.error,select.error{color:red;border:1px solid red!important;}.error::-webkit-input-placeholder{color:red!important;opacity:1;}.error:-moz-placeholder{color:red!important;opacity:1;}.select2.error+label.error{position:absolute;bottom:0;}.select2.error~.select2-container{margin-bottom:24px;}.select2.error~.select2-container .select2-selection{border-color:red;}.select2.error~.select2-container .select2-selection__rendered{color:red;}input[type="checkbox"].error~span{color:red;}label.error,.wpcf7-not-valid-tip{color:red;font-size:14px;}label.wpcf7-not-valid-tip ~ .wpcf7-not-valid-tip, label.error ~ .wpcf7-not-valid-tip{display:none;}</style>');
+jQuery('head').append('<style type="text/css">input.error,textarea.error,select.error{color:red;border:1px solid red!important;}.error::-webkit-input-placeholder{color:red!important;opacity:1;}.error:-moz-placeholder{color:red!important;opacity:1;}.select2.error+label.error{position:absolute;bottom:0;}.select2.error~.select2-container{margin-bottom:24px;}.select2.error~.select2-container .select2-selection{border-color:red;}.select2.error~.select2-container .select2-selection__rendered{color:red;}input[type="checkbox"].error~span{color:red;}label.error,.wpcf7-not-valid-tip{color:red;font-size:14px;}label.wpcf7-not-valid-tip ~ .wpcf7-not-valid-tip, label.error ~ .wpcf7-not-valid-tip{display:none;}</style>');
 
 var yUrl = new URL(document.currentScript.src);
 var yLang = yUrl.searchParams.get("lang");
@@ -11,7 +11,8 @@ var y_translations = {
 	minimum				: 'Minimum',
 	letters				: 'letters',
 	numbers				: 'numbers',
-	numbers_only		: 'Numbers only'
+	numbers_only		: 'Numbers only',
+	is					: 'is'
 };
 if(yLang == 'he' || yLang == 'he-IL'){
 	yLang = 'he';
@@ -22,25 +23,26 @@ if(yLang == 'he' || yLang == 'he-IL'){
 		minimum				: 'מינימום',
 		letters				: 'אותיות',
 		numbers				: 'מספרים',
-		numbers_only		: 'מספרים בלבד'
+		numbers_only		: 'מספרים בלבד',
+		is					: 'הינו'
 	};
 }
 
 // Key up required
-jQuery('body').on('keyup','.required, .wpcf7-validates-as-required', function(){
-	y_validate_field( jQuery(this), 'keyup' );
+jQuery('body').on('keyup', '.required, .wpcf7-validates-as-required', function(){
+	y_check_req( jQuery(this) );
 });
-// Key up not required
-jQuery('body').on('keyup','[minlength], [type="tel"]', function(){
-	if( ! y_check_if_number( jQuery(this) ) ) {
-		return false;
-	}
-	if( ! y_check_minlength( jQuery(this) ) ) {
-		return false;
-	}
+// Key up digits
+jQuery('body').on('keyup', '[type="tel"]', function(){
+	y_check_if_number( jQuery(this) );
+});
+// Key up minlength
+jQuery('body').on('keyup','[minlength]', function(){
+	y_check_minlength( jQuery(this) );
 });
 // Blur required
-jQuery('body').on('blur','.required, .wpcf7-validates-as-required', function(){
+jQuery('body').on('blur', '.required, .wpcf7-validates-as-required', function(){
+	y_check_req( jQuery(this) );
 	if( ! jQuery(this).hasClass('password-confirm') ) {
 		y_validate_field(jQuery(this), 'blur', function( field ) {
 			if( typeof y_blur_after_validate === 'function' ) {
@@ -50,9 +52,18 @@ jQuery('body').on('blur','.required, .wpcf7-validates-as-required', function(){
 	}
 });
 // Blur password-confirm
-jQuery('body').on('blur','.password-confirm', function(){
-	if( jQuery(this).val() !== '' )
-		y_validate_field( jQuery(this), 'blur' );
+jQuery('body').on('blur', '.password-confirm', function(){
+	if( jQuery(this).val() !== '' ) {
+		y_password_confirm( jQuery(this) );
+	}
+});
+// Blur Email
+jQuery('body').on('blur', '[type="email"]', function(){
+	y_check_email( jQuery(this) );
+});
+// Blur digits
+jQuery('body').on('blur', '[type="tel"]', function(){
+	y_check_if_number( jQuery(this) );
 });
 // Submit form
 jQuery('body').on('submit', 'form', function(e){
@@ -79,29 +90,86 @@ function y_validate_form( form ) {
 	}
 	return valid;
 };
+function get_placeholder( field ) {
+	var placeholder = '';
+	if( field.attr('placeholder') ) {
+		placeholder = field.attr('placeholder');
+	} else if( field.attr('id') && jQuery('label[for="'+field.attr('id')+'"]').length ) {
+		placeholder = jQuery('label[for="'+field.attr('id')+'"]:not(.error)').text();
+	}
+	return placeholder;
+};
 // Validate field
 function y_validate_field( field, function_type, callback ) {
-	if( ! y_check_if_number( field ) ) {
+
+	// required
+	if( ! y_check_req( field ) ) {
+		if (callback && typeof callback === 'function') {
+			callback( field );
+		}
 		return false;
 	}
-	if( ! y_check_minlength( field ) ) {
+	
+	// digits
+	if( ! y_check_if_number( field ) ) {
+		if (callback && typeof callback === 'function') {
+			callback( field );
+		}
 		return false;
 	}
 	
 	// email
+	if( ! y_check_email( field ) ) {
+		if (callback && typeof callback === 'function') {
+			callback( field );
+		}
+		return false;
+	}
+	
+	// minlength
+	if( ! y_check_minlength( field ) ) {
+		if (callback && typeof callback === 'function') {
+			callback( field );
+		}
+		return false;
+	}
+	
+	// password-confirm
+	if( ! y_password_confirm( field ) ) {
+		if (callback && typeof callback === 'function') {
+			callback( field );
+		}
+		return false;
+	}
+};
+// Email
+function y_check_email( field, callback ){
 	if( field.val() !== '' && ( field.attr('type') === 'email' || field.hasClass('email') ) ) {
 		if( /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test( field.val() ) ) { 
 			y_remove_error_msg( field );
-			if( callback != undefined && callback != null ) {
+			// return true;
+		} else {
+			var msg = y_translations.invalid_email;
+			var placeholder = get_placeholder( field );
+			if( placeholder ) {
+				msg = placeholder + ': ' + msg;
+			}
+			y_add_error_msg( field, msg );
+			
+			if (callback && typeof callback === 'function') {
 				callback( field );
 			}
-			return true;
-		} else {
-			y_add_error_msg( field, y_translations.invalid_email );
 			return false;
+			// errors = true;
 		};
 	}
-	// password-confirm
+	if (callback && typeof callback === 'function') {
+		callback( field );
+	}
+	return true;
+}
+// password-confirm
+function y_password_confirm( field, callback ){
 	if( field.hasClass('password-confirm') ) {
 		var confirm_fields = field.closest('form').find('.password-confirm');
 		var not_empty = 0;
@@ -113,47 +181,65 @@ function y_validate_field( field, function_type, callback ) {
 		if( not_empty > 1 ) {
 			if( field.val() !== confirm_fields.not( field ).val() ) {
 				y_add_error_msg( confirm_fields, y_translations.passwords_not_match );
-				return false;
-			} else {
-				y_remove_error_msg( confirm_fields );
-				if( callback != undefined && callback != null ) {
+				if (callback && typeof callback === 'function') {
 					callback( field );
 				}
-				return true;
+				return false;
+				// errors = true;
+			} else {
+				y_remove_error_msg( confirm_fields );
+				// return true;
 			}
 		}
 	}
-	// required
+	if (callback && typeof callback === 'function') {
+		callback( field );
+	}
+	return true;
+};
+// Required
+function y_check_req( field, callback ){
 	if( field.hasClass('required') || field.hasClass('wpcf7-validates-as-required') ) {
 		if( field.val() === '' || field.siblings('.wpcf7-not-valid-tip').length ) {
 			if( field.siblings('.wpcf7-not-valid-tip').length ) {
 				y_remove_error_msg( field );
 			} else {
-				y_add_error_msg( field, y_translations.required_field );
+				var msg = y_translations.required_field;
+				var placeholder = get_placeholder( field );
+				if( placeholder ) {
+					msg = placeholder + ' ' + y_translations.is + ' ' + msg;
+				}
+				y_add_error_msg( field, msg );
+			}
+			if (callback && typeof callback === 'function') {
+				callback( field );
 			}
 			return false;
 		} else if( field.hasClass('required') || field.hasClass('wpcf7-validates-as-required') ) {
 			y_remove_error_msg( field );
-			if( callback != undefined && callback != null ) {
-				callback( field );
-			}
-			return true;
 		}
+	}
+	if (callback && typeof callback === 'function') {
+		callback( field );
 	}
 	return true;
 };
-//check if number field
+// Number
 function y_check_if_number( field ) {
 	if( field.attr('type') == 'tel' && /[^0-9]/.test( field.val() ) ) {
-		y_add_error_msg( field, y_translations.numbers_only );
+		var msg = y_translations.numbers_only;
+		var placeholder = get_placeholder( field );
+		if( placeholder ) {
+			msg = placeholder + ': ' + msg;
+		}
+		y_add_error_msg( field, msg );
 		return false;
 	} else {
 		y_remove_error_msg( field );
-		return true;
 	}
 	return true;
 }
-//minlength
+// minlength
 function y_check_minlength( field ) {
 	if( field.attr('minlength') ) {
 		var min = field.attr('minlength');
@@ -162,11 +248,15 @@ function y_check_minlength( field ) {
 			if( field.attr('type') == 'tel' ) {
 				min_word = y_translations.numbers;
 			}
-			y_add_error_msg( field, y_translations.minimum+' '+min+' '+min_word );
+			var msg = y_translations.minimum + ' ' + min + ' ' + min_word;
+			var placeholder = get_placeholder( field );
+			if( placeholder ) {
+				msg = placeholder + ': ' + msg;
+			}
+			y_add_error_msg( field, msg );
 			return false;
 		} else {
 			y_remove_error_msg( field );
-			return true;
 		}
 	}
 	return true;
@@ -180,6 +270,14 @@ function y_add_error_msg( self, msg ) {
 	if( self.next('label.error').length ) {
 		self.addClass('error').next('label.error').text( msg ).show();
 	} else {
-		self.addClass('error').after('<label class="error" style="display:block;">'+msg+'</label>');
+		var forid = '';
+		if( self.attr('id') ) {
+			forid = 'for="'+self.attr('id')+'"';
+		}
+		self.addClass('error').after('<label '+forid+' class="error" style="display:block;">'+msg+'</label>');
 	}
 };
+//Disable cf7 validation
+document.addEventListener('wpcf7submit', function(event) {
+	event.detail.apiResponse.invalid_fields = [];
+});
